a91e0098bc6ddeb3dbbd7d818491fd66e8db9fd9
victorpuch <victor_rock_rdk@hotmail.com>
2020-06-12 17:29:33 -0500

    prueba gitlab 2 mirroring

--- tareasold.php
+++ tareasold.php
@@ -18,6 +18,9 @@
  <head>
 <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
 <meta charset="utf-8" />
+
+
+
 <title><?php print($titulo); ?> | FCH Tareas</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 <meta content="" name="description" />
@@ -193,7 +196,7 @@ if($check > 0){ //La tarea tiene subtareas
 </tr>
   </div>
 
-	
+
 <?php
 }
 ?>
